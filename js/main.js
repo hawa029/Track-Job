61ebd84bc77896bf04ff2eb7b1663463f23546fb
Jean Paul SOSSAH <jpaulcamillo@outlook.fr>
2021-11-29 21:10:24 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/*function onload(){
+     // create a couple of elements in an otherwise empty HTML page
+    const heading = document.createElement("h1");
+    const heading_text = document.createTextNode("TRACK JOB");
+    heading.appendChild(heading_text);
+    document.body.appendChild(heading);
+    //adding the h1 element to the header
+    const header = document.querySelector('header');
+    header.appendChild(heading_text);
+} 
+onload()
+*/
+
+
